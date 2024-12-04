d493fc376fdbc554a130723c227c1bf7fe16aa1b
sai-uday1 <161728136+sai-uday1@users.noreply.github.com>
2024-12-04 21:51:25 +0530

    Template / Snippet I use for CP.

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) ((void)0)
+#endif
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	return 0;
+}
+
